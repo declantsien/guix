77ef282e030839742279a03ab27e4b0a32d1e556
Maxim Cournoyer <maxim.cournoyer@gmail.com>
2023-10-09 11:35:26 -0400

    gnu: seabios-qemu: Import the correct set of modules.
    * gnu/packages/engineering.scm (seabios-qemu) [arguments]
    <modules>: Replace %default-gnu-imported-modules with %default-gnu-modules.
    
    Change-Id: I83e0768198c81020245b9281c678733803824236

--- gnu/packages/firmware.scm
+++ gnu/packages/firmware.scm
@@ -664,7 +664,7 @@ coreboot.")
     (supported-systems %supported-systems)
     (arguments
      (substitute-keyword-arguments (package-arguments seabios)
-       ((#:modules modules %default-gnu-imported-modules)
+       ((#:modules modules %default-gnu-modules)
         `((ice-9 match)
           (ice-9 threads)
           ,@modules))
