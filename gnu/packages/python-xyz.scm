19c4362becb998a0468e84a47b0ab62f9fc52318
Ludovic Courtès <ludo@gnu.org>
2024-01-17 11:06:04 +0100

    gnu: meson: Update to 1.1.1 and ungraft.
    * gnu/packages/build-tools.scm (meson): Update to 1.1.1.
    [replacement]: Remove.
    (meson/newer): Remove.
    (meson-python/newer): Remove.
    * gnu/packages/pdf.scm (capypdf)[arguments]: Remove #:meson.
    
    Change-Id: I77dd9203461e8dd635753ec742bd8046dd63e3f9
    * gnu/packages/python-science.scm (python-pandas-2): Use regular meson-python.
    * gnu/packages/python-xyz.scm (python-contourpy): Same

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -1291,7 +1291,7 @@ into dataclasses.")
      (list python-numpy))
     (native-inputs
      (list cmake
-           meson-python/newer
+           meson-python
            pkg-config
            pybind11
            python-pytest
