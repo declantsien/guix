14e18d5f8f9c36b979d9df71376164e2e0ef0ecc
Josselin Poiret <dev@jpoiret.xyz>
2024-02-02 19:05:05 +0100

    gnu: inetutils: Add libxcrypt dependency.
    * gnu/packages/admin.scm (inetutils): Add libxcrypt dependency.
    
    Change-Id: I6c7bc49f60e532edac395a2b1ba89e3f1f95b4e7

--- gnu/packages/admin.scm
+++ gnu/packages/admin.scm
@@ -993,6 +993,7 @@ re-executing them as necessary.")
     (inputs
      (list coreutils
            shadow                     ;for login (used in telnetd and rlogind)
+           libxcrypt
            ncurses
            readline))                   ;for 'ftp'
     (native-inputs
