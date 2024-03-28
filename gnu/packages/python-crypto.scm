aa02159ea73dfeca538f7a16fc7b9d1f962836a9
Efraim Flashner <efraim@flashner.co.il>
2024-03-28 17:42:18 +0200

    gnu: python-cryptography: Update to 42.0.5.
    * gnu/packages/python-crypto.scm (python-cryptography-vectors): Update
    to 42.0.5.
    [native-inputs]: Add python-flit-core.
    (python-cryptography): Update to 42.0.5.
    [arguments]: Adjust 'disable-rust-extension-build phase.
    [native-inputs]: Add python-certifi.  Remove python-hypothesis,
    python-subtests.
    (python-cryptography-rust)[cargo-inputs]: Add rust-cfg-if-1,
    rust-self-cell-1.  Replace rust-pem-1 with 3, rust-pyo3-0.18 with 0.20.
    Remove rust-ouroboros-0.15.
    
    Change-Id: I1d17ee8ca60eaa65a86b2f6f8054391dda4201c8

--- gnu/packages/python-crypto.scm
+++ gnu/packages/python-crypto.scm
@@ -1,6 +1,6 @@
 ;;; GNU Guix --- Functional package management for GNU
 ;;; Copyright © 2015 Eric Dvorsak <eric@dvorsak.fr>
-;;; Copyright © 2015-2021, 2023 Efraim Flashner <efraim@flashner.co.il>
+;;; Copyright © 2015-2021, 2023, 2024 Efraim Flashner <efraim@flashner.co.il>
 ;;; Copyright © 2015, 2016, 2017, 2019 Leo Famulari <leo@famulari.name>
 ;;; Copyright © 2016, 2017, 2020, 2022, 2023 Marius Bakke <marius@gnu.org>
 ;;; Copyright © 2017 Ben Sturmfels <ben@sturm.com.au>
@@ -487,16 +487,18 @@ is used by the Requests library to verify HTTPS requests.")
 (define-public python-cryptography-vectors
   (package
     (name "python-cryptography-vectors")
-    (version "41.0.4")
+    (version "42.0.5")
     (source
      (origin
        (method url-fetch)
        (uri (pypi-uri "cryptography_vectors" version))
        (sha256
         (base32
-         "17m1azxfc3w0390jp9mkx1v0k3xfv4v1lrgch4hvbbm77s0z42j4"))))
+         "1lcflcvv0xjz5cyvf33iav1vd91qzjvl4w7h4qaxlcnbn3ixap2h"))))
     (build-system pyproject-build-system)
     (arguments (list #:tests? #f))  ; No tests included.
+    (native-inputs
+     (list python-flit-core))
     (home-page "https://github.com/pyca/cryptography")
     (synopsis "Test vectors for the cryptography package")
     (description
@@ -507,14 +509,14 @@ is used by the Requests library to verify HTTPS requests.")
 (define-public python-cryptography
   (package
     (name "python-cryptography")
-    (version "41.0.4")
+    (version "42.0.5")
     (source
      (origin
        (method url-fetch)
        (uri (pypi-uri "cryptography" version))
        (sha256
         (base32
-         "06pm952pr6f31pzwh5fb68zryqyss0hg9cbggxm15z15844w7svz"))))
+         "1qdz0yk5smi0dnywbxmanccwizilmnzgbbihjpmpgm6zjpn7xq3g"))))
     (build-system pyproject-build-system)
     (arguments
      (list
@@ -525,8 +527,7 @@ is used by the Requests library to verify HTTPS requests.")
                        ;; 'python-cryptography-rust', so there's no need
                        ;; to build it here.
                        (substitute* "pyproject.toml"
-                         ((".*setuptools-rust.*") ""))
-                       (delete-file "setup.py")))
+                         (("\\s+\\\"setuptools-rust.*") ""))))
                    (add-before 'check 'symlink-rust-library
                      (lambda* (#:key inputs outputs #:allow-other-keys)
                        (symlink (search-input-file
@@ -536,13 +537,12 @@ is used by the Requests library to verify HTTPS requests.")
                                                "_rust.abi3.so")))))))
 
     (native-inputs
-     (list python-cryptography-vectors
-           python-hypothesis
+     (list python-certifi
+           python-cryptography-vectors
            python-iso8601
            python-pretend
            python-pytest                ;for subtests
-           python-pytest-benchmark
-           python-pytest-subtests))
+           python-pytest-benchmark))
     (inputs (list python-cryptography-rust))
     (propagated-inputs (list python-cffi))
     (home-page "https://github.com/pyca/cryptography")
@@ -596,14 +596,15 @@ ciphers, message digests and key derivation functions.")
       #:cargo-inputs
       `(("rust-asn1" ,rust-asn1-0.15)
         ("rust-cc" ,rust-cc-1)
+        ("rust-cfg-if" ,rust-cfg-if-1)
         ("rust-foreign-types" ,rust-foreign-types-0.3)
         ("rust-foreign-types-shared" ,rust-foreign-types-shared-0.1)
         ("rust-once-cell" ,rust-once-cell-1)
         ("rust-openssl" ,rust-openssl-0.10)
         ("rust-openssl-sys" ,rust-openssl-sys-0.9)
-        ("rust-ouroboros" ,rust-ouroboros-0.15)
-        ("rust-pem" ,rust-pem-1)
-        ("rust-pyo3" ,rust-pyo3-0.18))))
+        ("rust-pem" ,rust-pem-3)
+        ("rust-pyo3" ,rust-pyo3-0.20)
+        ("rust-self-cell" ,rust-self-cell-1))))
     (native-inputs (list pkg-config python python-cffi))
     ;; XXX: Adding rust-openssl-sys-0.9 is needed because #:cargo-inputs
     ;; doesn't honor propagated-inputs.
